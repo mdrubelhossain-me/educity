6c1271552e362b0d014b753be05683819d8ce07e
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-10-13 00:12:33 +0600

    hero section addedd

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,8 +4,12 @@ import './HeroResponsive.css'
 
 const Hero = () => {
     return (
-        <div className='hero'>
-            
+        <div className='hero container'>
+            <div className="hero-text">
+                <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore atque ea quod perferendis error eaque voluptates aliquam rerum</p>
+                <button className='btn'>Explore More	&rarr;</button>
+            </div>
         </div>
     );
 };
