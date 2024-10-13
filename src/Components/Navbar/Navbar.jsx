f713e38f076d2d557d1b96bffb562d60a0fc24ec
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2024-10-14 02:24:55 +0600

    make nav responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../Navbar/Navbar.css";
 import "../Navbar/NavbarResponsive.css";
 import { Link } from "react-scroll";
+import menu_icon from '../../assets/images/menu-icon.png'
 
 const Navbar = () => {
 
@@ -13,6 +14,11 @@ const Navbar = () => {
     })
   }, []);
 
+  const [mobileMenu, setMobilemenu] = useState(false);
+  const toggleMenu = ()=>{
+    mobileMenu ? setMobilemenu(false) : setMobilemenu(true);
+  }
+
 
   return (
     <div>
@@ -21,14 +27,15 @@ const Navbar = () => {
           <i class="fas fa-graduation-cap"></i>
           <span>EduCity</span>
         </div>
-        <ul>
+        <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-          <li><Link to='program' smooth={true} offset={0} duration={500}>Program</Link></li>
-          <li><Link to='about' smooth={true} offset={0} duration={500}>About Us</Link></li>
-          <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
-          <li><Link to='testimonial' smooth={true} offset={0} duration={500}>Testimonial</Link></li>
-          <li><Link to='contact' smooth={true} offset={0} duration={500} className="btn">Contact Us</Link></li>
+          <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
+          <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+          <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+          <li><Link to='testimonial' smooth={true} offset={-260} duration={500}>Testimonial</Link></li>
+          <li><Link to='contact' smooth={true} offset={-260} duration={500} className="btn">Contact Us</Link></li>
         </ul>
+        <img src={menu_icon} className="menu-icon" alt="menu-icon" onClick={toggleMenu}/>
       </nav>
     </div>
   );
